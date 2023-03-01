f6a46ab0017730ea28d304d92cb34df7f1467163
Uro <uriel.mallea@gmail.com>
2023-02-28 22:44:48 -0300

    cv argentina programa

---
+++ js/vinculo.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("hola mundo");
+    document.getElementById("demo").innerHTML = "Soy programador novato, especialista en reparacion de electrodomesticos y padre de dos hijos "
+}
